7aac40646748268405611f0f92076dbc8f7fff9e
София Николаева <NikolaevaSG040@mgpu.ru>
2021-01-01 16:02:13 +0300

    fix

--- exc4.py
+++ exc4.py
@@ -1,9 +1,8 @@
 #program to remove the dublicates from the list
-num = [1, 2, 1, 2, 3, 4, 5, 3]
-num.sort()
+num = [1, 2, 2, 2, 1, 2, 3, 4, 5, 3]
+only = []
+
 for i in num:
-    amount = 0
-    amount = num.count(i)
-    if amount > 1:
-        num.remove(i)
-print(num)
+   if i not in only:
+       only.append(i)
+print(only)
